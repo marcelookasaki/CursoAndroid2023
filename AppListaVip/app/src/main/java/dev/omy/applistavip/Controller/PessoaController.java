0d7f1b50ec0e2f36ed4ccff98af6a00c02ae18c3
Marcelo Yutaka Okasaki <marcelo.okasaki@gmail.com>
2023-07-20 18:49:33 -0300

    Readequado o código de mainActivity e PessoaController

--- AppListaVip/app/src/main/java/dev/omy/applistavip/Controller/PessoaController.java
+++ AppListaVip/app/src/main/java/dev/omy/applistavip/Controller/PessoaController.java
@@ -1,13 +1,26 @@
 package dev.omy.applistavip.Controller;
 
+import android.content.SharedPreferences;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 
 import dev.omy.applistavip.Model.Pessoa;
+import dev.omy.applistavip.View.MainActivity;
 
 public class PessoaController {
+    
+    // Declarar Shared Preferences
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor listaVIP;
 
+    public static final String NOME_PREFERENCES = "pref_listaVIP";
+
+    public PessoaController(MainActivity mainActivity) {
+        // Instancia Shared Preferences e configurar para edição
+        sharedPreferences  = mainActivity.getSharedPreferences(NOME_PREFERENCES, 0);
+        listaVIP = sharedPreferences.edit();
+    }
 
     @NonNull
     @Override
@@ -17,9 +30,36 @@ public class PessoaController {
 
         return super.toString();
     }
-
+    
     public void salvar(Pessoa pessoa) {
 
         Log.d("MVC_Controller", "Salvo " + pessoa.toString());
+
+        // Salva em shared preferences
+        listaVIP.putString("primeiroNome", pessoa.getPrimeiroNome());
+        listaVIP.putString("sobreNome", pessoa.getSobreNome());
+        listaVIP.putString("cursoDesejado", pessoa.getCursoDesejado());
+        listaVIP.putString("telefoneContato", pessoa.getTelefoneContato());
+        listaVIP.apply();
     }
+
+    public void recuperar(Pessoa pessoa){
+        // Recupera dados do shared preferences
+        pessoa.setPrimeiroNome(sharedPreferences.getString(
+                        "primeiroNome", ""));
+        pessoa.setSobreNome(sharedPreferences.getString(
+                "sobreNome", ""));
+        pessoa.setCursoDesejado(sharedPreferences.getString(
+                "cursoDesejado", ""));
+        pessoa.setTelefoneContato(sharedPreferences.getString(
+                        "telefoneContato", ""));
+    }
+
+    public void limpar(){
+        // Limpar shared preferences
+        listaVIP.clear();
+        listaVIP.apply();
+        
+    }
+
 }
