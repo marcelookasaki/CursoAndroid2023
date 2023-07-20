310efa8a3cfa97e246b8cbf49206573cff35970b
Marcelo Yutaka Okasaki <marcelo.okasaki@gmail.com>
2023-07-20 14:40:35 -0300

    Inserir valores ao objeto pessoa

--- AppListaVip/app/src/main/java/dev/omy/applistavip/View/MainActivity.java
+++ AppListaVip/app/src/main/java/dev/omy/applistavip/View/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.EditText;
 
 import dev.omy.applistavip.Model.Pessoa;
@@ -17,6 +18,10 @@ public class MainActivity extends AppCompatActivity {
     EditText etCursoDesejado;
     EditText etTelefone;
 
+    Button btnLimpar;
+    Button btnSalvar;
+    Button btnFinalizar;
+
     // Declara classe pessoa e cria objeto
     Pessoa pessoa;
 
@@ -25,8 +30,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Instanciar componentes
+        etPrimeiroNome = findViewById(R.id.etPrimeiroNome);
+        etSobrenome = findViewById(R.id.etSobrenome);
+        etCursoDesejado = findViewById(R.id.etCursoDesejado);
+        etTelefone = findViewById(R.id.etTelefone);
+        btnLimpar = findViewById(R.id.btnLimpar);
+        btnSalvar = findViewById(R.id.btnSalvar);
+        btnFinalizar = findViewById(R.id.btnFinalizar);
+
         // Instancia do objeto pessoa
         pessoa = new Pessoa();
+
+        // Inserir valores ao objeto
+        pessoa.setPrimeiroNome("Marcelo");
+        pessoa.setSobreNome("Okasaki");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefoneContato("11 97187-5153");
+
+        // Capturar dados
+        etPrimeiroNome.setText(pessoa.getPrimeiroNome());
+        etSobrenome.setText(pessoa.getSobreNome());
+        etCursoDesejado.setText(pessoa.getCursoDesejado());
+        etTelefone.setText(pessoa.getTelefoneContato());
 
         Log.i("POOAndroid", "Objeto pessoa: " + pessoa.toString());
 
