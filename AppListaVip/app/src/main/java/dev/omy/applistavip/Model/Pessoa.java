e57bdcca43ccd3a8342ffcf64b48aab18335fee6
Marcelo Yutaka Okasaki <marcelo.okasaki@gmail.com>
2023-07-20 12:55:31 -0300

    Criada a classe Pessoa camada model

---
+++ AppListaVip/app/src/main/java/dev/omy/applistavip/Model/Pessoa.java
@@ -0,0 +1,57 @@
+package dev.omy.applistavip.Model;
+
+public class Pessoa {
+
+    // Construtor
+    // Atributos
+    private String primeiroNome;
+    private String sobreNome;
+    private String cursoDesejado;
+    private String telefoneContato;
+
+    public Pessoa() {}
+
+    // Métodos de acesso - getters and setters
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+
+    public String getCursoDesejado() {
+        return cursoDesejado;
+    }
+
+    public void setCursoDesejado(String cursoDesejado) {
+        this.cursoDesejado = cursoDesejado;
+    }
+
+    public String getTelefoneContato() {
+        return telefoneContato;
+    }
+
+    public void setTelefoneContato(String telefoneContato) {
+        this.telefoneContato = telefoneContato;
+    }
+
+    // Método toString
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "primeiroNome='" + primeiroNome + '\'' +
+                ", sobreNome='" + sobreNome + '\'' +
+                ", cursoDesejado='" + cursoDesejado + '\'' +
+                ", telefoneContato='" + telefoneContato + '\'' +
+                '}';
+    }
+}
--- AppListaVip/app/src/main/java/dev/omy/applistavip/View/MainActivity.java
+++ AppListaVip/app/src/main/java/dev/omy/applistavip/View/MainActivity.java
@@ -3,14 +3,32 @@ package dev.omy.applistavip.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
 
+import dev.omy.applistavip.Model.Pessoa;
 import dev.omy.applistavip.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Declarar componentes da tela
+    EditText etPrimeiroNome;
+    EditText etSobrenome;
+    EditText etCursoDesejado;
+    EditText etTelefone;
+
+    // Declara classe pessoa e cria objeto
+    Pessoa pessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Instancia do objeto pessoa
+        pessoa = new Pessoa();
+
+        Log.i("POOAndroid", "Objeto pessoa: " + pessoa.toString());
+
     }
 }
